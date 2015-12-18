9aece7cdc2213d08d1cf7e47837d3d6b0648ca6f
vdkgithub <joseph.capio@gmail.com>
2015-12-18 15:34:43 +0800

    changes. again

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,18 @@ var Main = () => {
         _.mainBody.load('pages/projects.html');
     }
 
+    this.prClick = () => {
+        _.fetchJSON('menu', (data) => {
+            console.log(data);
+        })
+    }
+
+    this.fetchJSON = (a,o) => {
+        $.getJSON("jsonfiles/" + a + '.json', function (data) {
+            o(data);
+        })
+    }
+
 
 }
 
