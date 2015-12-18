f606f9f773a9fdd7b99ede805b989b97ec584046
vdkgithub <joseph.capio@gmail.com>
2015-12-18 15:42:27 +0800

    err

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,25 +1,50 @@
 
 var Main = () => {
     var _ = this;
-    
-    this.mainBody = $('div.main-body');
 
-    this.menuClick = (d, e) => {
-        _.mainBody.load('pages/projects.html');
+    this.mData = {
+        Base: {},
+        prMenus: ko.observableArray(),
+        CurrentMenu: ko.observable(''),
+        CurrentProject: ko.observable()
     }
 
-    this.prClick = () => {
-        _.fetchJSON('menu', (data) => {
-            console.log(data);
+    this.mainBody = $('div.main-body');
+
+
+    this.menuClick = (b, d, e) => {
+        _.mData.CurrentMenu(b);
+        $('div.main-body').css({ opacity: 0 });
+        if (b == "Projects") {
+            _.mData.prMenus(Enumerable.From(_.mData.Base.Projects).Select(r=> { return { Name: r.Name, ID: r.ID } }).ToArray());
+        }
+        setTimeout(() => {
+            $('div.main-body').css({ opacity: 1 });
         })
     }
 
-    this.fetchJSON = (a,o) => {
+    this.transition = (o) => {
+    }
+
+    this.Init = () => {
+        _.fetchJSON('projects', (data) => {
+            _.mData.Base = data;
+        });
+    }
+
+    this.prClick = (d, e) => {
+        _.mData.CurrentProject(Enumerable.From(_.mData.Base.Projects).Where(r=>r.ID == d.ID).SingleOrDefault(null));
+    }
+
+    this.fetchJSON = (a, o) => {
         $.getJSON("jsonfiles/" + a + '.json', function (data) {
             o(data);
         })
     }
 
+    var __construct = function() {
+        _.Init();
+    }(_);
 
 }
 
